6613674546f69ecac3a96a2ec99c908d394fe48c
bothomthit <125108405+bothomthit@users.noreply.github.com>
2023-05-25 16:08:43 +0700

    danhSach.java

--- danhSach.java
+++ danhSach.java
@@ -37,6 +37,61 @@ public class danhSach {
         for(phongHoc Phonghoc:list){
             System.out.printf("%20s| %.2f\n",Phonghoc.getMaPhong(),Phonghoc.getDayNha(), Phonghoc.getSoBongDen(),Phonghoc.getDienTich(),Phonghoc.getBongDenTB());
     }
+          public void sapTheoDayNha() {
+        Collections.sort(danhSach, (phong1, phong2) -> phong1.getDayNha().compareTo(phong2.getDayNha()));
+        System.out.println("danh sach da duoc xep.");
+    }
+
+    public void sapTheoDienTich() {
+        Collections.sort(danhSach, (phong1, phong2) -> Double.compare(phong1.getDienTich(), phong2.getDienTich()));
+        System.out.println("danh sach da duoc xep");
+    }
+
+    public void sapTheoSoBongDen() {
+        Collections.sort(danhSachPhong, (phong1, phong2) -> Integer.compare(phong1.getSoBongDen(), phong2.getSoBongDen()));
+        System.out.println("danh sach da duoc xep");
+    }
+
+    public void capNhatSoMayTinh(String maPhong, int soMayTinh) {
+        for (phongHoc phong : danhSach) {
+            if (phong instanceof PhongMayTinh && phong.getMaPhong().equals(maPhong)) {
+                ((PhongMayTinh) phong).setSoMayTinh(soMayTinh);
+                System.out.println("danh sach da duoc xep.");
+                return;
+            }
+        }
+        System.out.println(" " + maPhong);
+    }
+
+    public void xoaPhong(String maPhong) {
+        Iterator<phongHoc> iterator = danhSach.iterator();
+        while (iterator.hasNext()) {
+            phongHoc phong = iterator.next();
+            if (phong.getMaPhong().equals(maPhong)) {
+                iterator.remove();
+                System.out.println("xoa thanh cong.");
+                return;
+            }
+        }
+        System.out.println( " null " + maPhong);
+    }
+
+    public void inTongSoPhong() {
+        System.out.println("Tong so phong: " + danhSach.size());
+    }
+
+    public void inDanhSachPhongMay() {
+        boolean found = false;
+        for (phongHoc phong : danhSach) {
+            if (phong instanceof phongMayTinh && ((phongMayTinh) phong).getSoMayTinh() == 60) {
+                System.out.println(phong);
+                found = true;
+            }
+        }
+        if (!found) {
+            System.out.println("khong tim thay");
+        }
+    }
     }
  
     }
